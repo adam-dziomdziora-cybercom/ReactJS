9b5e3f6b4dcd254cbe4c0a9d9efdaab2fafb08d0
Adam Dziomdziora <adam.dziomdziora@dokt.p.lodz.pl>
2020-02-28 14:54:17 +0100

    list 1

--- src/index.js
+++ src/index.js
@@ -3,7 +3,28 @@ import ReactDOM from 'react-dom';
 import './style/main.less';
 
 const HelloWorld = props => {
-  return <h1 className="header">Hello World!</h1>;
+  return (
+    <div>
+      <h1 className="header">Hello World!</h1>
+      <p>I will display &#9749;</p>
+      <div id="injectHere" />
+      <div id="andHere" />
+    </div>
+  );
 };
 
 ReactDOM.render(<HelloWorld />, document.getElementById('root'));
+
+ReactDOM.render(
+  React.createElement('h2', null, 'Playing with react is cool'),
+  document.getElementById('injectHere')
+);
+
+ReactDOM.render(
+  React.createElement(
+    'ul',
+    { id: 'myList' },
+    React.createElement('li', { id: 'item1' }, 'item 1 hi \u26CF;')
+  ),
+  document.getElementById('andHere')
+);
