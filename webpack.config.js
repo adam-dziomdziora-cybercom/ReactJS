f991d64964bc793925562302ca360997d153a936
CYBERDOM2\addzi1 <adam.dziomdziora@cybercom.com>
2022-06-30 10:53:35 +0200

    fixes

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
     publicPath: '/',
     filename: 'bundle.js',
   },
-  devServer: { contentBase: './build' },
+  devServer: { contentBase: './dist' },
   module: {
     rules: [
       {
